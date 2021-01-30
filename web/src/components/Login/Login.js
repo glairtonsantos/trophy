c21140525c55d853ae1f7f16b93d612029feee9f
Glairton Santos <j.glairtonewton@alu.ufc.br>
2021-01-30 09:45:53 -0300

    chore: add border in login

--- web/src/components/Login/Login.js
+++ web/src/components/Login/Login.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
-import { Button, Form, Row, Col } from 'react-bootstrap';
+import { Button, Form, Row, Col, Container } from 'react-bootstrap';
 
 import API from '../../api';
 import './Login.css';
@@ -35,33 +35,35 @@ export default function Login({ setToken }) {
   }
 
   return(
-    <div className="login-wrapper">
-      <h1>Please Log In</h1>
-    <Form onSubmit={handleSubmit}>
-      <Form.Group as={Row} controlId="formHorizontalEmail">
-          <Form.Label>
-          Username
-          </Form.Label>
-          <Col>
-            <Form.Control type="text" placeholder="Username"  onChange={e => setUserName(e.target.value)} />
-          </Col>
-      </Form.Group>
-      
-      <Form.Group as={Row} controlId="formHorizontalPassword">
-          <Form.Label>
-            Password
-          </Form.Label>
-          <Col>
-            <Form.Control type="password" placeholder="Password"  onChange={e => setPassword(e.target.value)} />
-          </Col>
-      </Form.Group>
-      <Form.Group className="login-btn">
-        <Button variant="primary" type="submit">
-          Login
-        </Button>
-      </Form.Group>
-    </Form>
-    </div>
+    <Container>
+      <div className="login-wrapper">
+        <h1>Please Log In</h1>
+        <Form onSubmit={handleSubmit} className="login-div">
+          <Form.Group as={Row} controlId="formHorizontalEmail">
+              <Form.Label>
+              Username
+              </Form.Label>
+              <Col>
+                <Form.Control type="text" placeholder="Username"  onChange={e => setUserName(e.target.value)} />
+              </Col>
+          </Form.Group>
+          
+          <Form.Group as={Row} controlId="formHorizontalPassword">
+              <Form.Label>
+                Password
+              </Form.Label>
+              <Col>
+                <Form.Control type="password" placeholder="Password"  onChange={e => setPassword(e.target.value)} />
+              </Col>
+          </Form.Group>
+          <Form.Group className="login-btn">
+            <Button variant="primary" type="submit">
+              Login
+            </Button>
+          </Form.Group>
+        </Form>
+      </div>
+    </Container>
   )
 }
 
